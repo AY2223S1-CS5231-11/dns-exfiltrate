90ac0ab852e617d3dcb986d6ee6a88299651528e
Ian Yong <ianyongyc@gmail.com>
2022-11-21 00:02:42 +0800

    Add parsing of nameserver argument to client

--- client/parser/arguments.go
+++ client/parser/arguments.go
@@ -8,12 +8,14 @@ import (
 )
 
 type Arguments struct {
-	Filename string
+	NameServer string
+	Filename   string
 }
 
 func ParseArgs() *Arguments {
 	parser := argparse.NewParser("DNS Data Exfiltration Client", "Client for exfiltrating data over DNS")
 
+	nameServer := parser.String("n", "nameserver", &argparse.Options{Required: true, Help: "Address of the nameserver to exfiltrate to"})
 	filename := parser.String("f", "filename", &argparse.Options{Required: true, Help: "Name of file to exfiltrate"})
 
 	err := parser.Parse(os.Args)
@@ -23,6 +25,7 @@ func ParseArgs() *Arguments {
 	}
 
 	return &Arguments{
-		Filename: *filename,
+		NameServer: *nameServer,
+		Filename:   *filename,
 	}
 }
