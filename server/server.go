5d7f379c020b3e1147d12d843fc37e8ee8d8f4d6
Ian Yong <ianyongyc@gmail.com>
2022-11-20 23:37:13 +0800

    Add listening on UDP port 53

--- server/server.go
+++ server/server.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("Starting server...")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Stopping server...")
+		os.Exit(0)
+	}()
+
+	udpServer, err := net.ListenPacket("udp", ":53")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer udpServer.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		_, _, err := udpServer.ReadFrom(buf)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println(buf)
+	}
 }
