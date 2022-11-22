5453cf0431204331cd435810acfa1c67c89bdd35
Ian Yong <ianyongyc@gmail.com>
2022-11-22 13:24:14 +0800

    Move max domain name length constant into DNS exfiltrator file

--- client/client.go
+++ client/client.go
@@ -5,14 +5,6 @@ import (
 	"dns-exfiltration-client/parser"
 )
 
-const (
-	// To simplify implementations, the total length of a domain name (i.e.,
-	// label octets and label length octets) is restricted to 255 octets or
-	// less.
-	// - https://www.rfc-editor.org/rfc/rfc1035#section-3.1
-	MAX_DOMAIN_NAME_LENGTH = 255
-)
-
 func main() {
 	args := parser.ParseArgs()
 	dnsExfiltrator := exfiltrator.NewDnsExfiltrator(args.NameServer)
--- client/exfiltrator/dns.go
+++ client/exfiltrator/dns.go
@@ -6,6 +6,14 @@ import (
 	"net"
 )
 
+const (
+	// To simplify implementations, the total length of a domain name (i.e.,
+	// label octets and label length octets) is restricted to 255 octets or
+	// less.
+	// - https://www.rfc-editor.org/rfc/rfc1035#section-3.1
+	MAX_DOMAIN_NAME_LENGTH = 255
+)
+
 type dnsExfiltrator struct {
 	NameServer string
 }
