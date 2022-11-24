1ec817484c75217ecd16174b4d7a36126a182f57
Ian Yong <ianyongyc@gmail.com>
2022-11-24 14:32:39 +0800

    Fix crash when filename contains uncreated directories

--- server/fileutils/fileutils.go
+++ server/fileutils/fileutils.go
@@ -4,12 +4,13 @@ import (
 	"errors"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func CreateDirIfNotExists(path string) {
 	_, err := os.Stat(path)
 	if errors.Is(err, os.ErrNotExist) {
-		err = os.Mkdir(path, 0775)
+		err = os.MkdirAll(path, 0775)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -17,6 +18,8 @@ func CreateDirIfNotExists(path string) {
 }
 
 func CreateFileIfNotExists(path string) *os.File {
+	dir := filepath.Dir(path)
+	CreateDirIfNotExists(dir)
 	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0664)
 	if err != nil {
 		log.Fatalln(err)
